089bc09c4961d9456c22d64520c8a93e4306f4b3
Marcia Villalba <marciavillalba@Marcias-MacBook-Pro.local>
2018-08-27 21:49:43 +0300

    use plugin

---
+++ bye.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports.bye = () => {
+	console.log('bye');
+};
---
+++ hello.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var array = require('lodash/array');
+
+module.exports.hello = () => {
+	console.log('hola');
+};
